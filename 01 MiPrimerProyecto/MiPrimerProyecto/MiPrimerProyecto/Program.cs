7e4bcdfc761dced30871e1cc79fd4551d44ecd50
RubenFort <rdfortu@gmail.com>
2021-07-28 00:09:20 +0200

    Primer proyecto, variables, leer e imprimir por pantalla. Segundo proyecto, paso por valor y paso por referencia

---
+++ 01 MiPrimerProyecto/MiPrimerProyecto/MiPrimerProyecto/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MiPrimerProyecto
+{
+    class Program
+    {
+        /// <summary>
+        /// Método principal
+        /// </summary>
+        /// <param name="args"> descripción del parámetro args </param>
+        static void Main(string[] args)
+        {
+            #region Imprimir
+            Console.WriteLine("Hola Mundo!"); //Imprime por pantalla
+            #endregion
+            int num = 10;
+            string nombre = "Rubén";
+            var apellido = "López";
+
+            Console.WriteLine("Ingresa tú nombre: ");
+            nombre = Console.ReadLine(); //Leer linea por pantalla
+            Console.WriteLine("Tú nombre es: " + nombre);
+
+            //Tipos de datos
+            byte miByte = 3;
+            int positivo = 500;
+            int negativo = -800;
+            float miFloat = 23.12f;
+            double miDouble = 123.12;
+            decimal miDecimal = 12.456m;
+            bool miBool = true;
+            string miString = "cadena de texto";
+            char miChar = 'a';
+
+            #region ReadK
+            Console.ReadKey(); //Espera a que el usuario pulse una tecla
+            #endregion
+        }
+    }
+}
---
+++ 02Coordenadas/02Coordenadas/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _02Coordenadas
+{
+    class Program
+    {
+        public class Coordenadas { public int x; public int y; }
+
+        static void Main(string[] args)
+        {
+            #region tipoXValor
+            bool val = false;
+            bool nuevoVal = val;
+            val = true;
+
+            Console.WriteLine("val {0}", val);
+            Console.WriteLine("nuevoVal {0}", nuevoVal);
+            #endregion
+
+            #region tipoXReferencia
+            Coordenadas coordA = new Coordenadas();
+            coordA.x = 1;
+            coordA.y = 50;
+
+            Coordenadas coordB = coordA;
+
+            Console.WriteLine("coordA.x {0}", coordA.x);
+            Console.WriteLine("coordA.y {0}", coordA.y);
+            Console.WriteLine("coordB.x {0}", coordB.x);
+            Console.WriteLine("coordB.y {0}", coordB.y);
+
+            coordA.x = 100;
+            Console.WriteLine("coordB.x {0}", coordB.x);
+            Console.WriteLine("coordB.y {0}", coordB.y);
+            #endregion
+
+            #region keyboardRef
+            int miNum = 10;
+            AddTen(ref miNum);
+            Console.WriteLine("miNum {0}", miNum);
+            Console.ReadKey();
+            #endregion
+        }
+
+        static void AddTen (ref int number)
+        {
+            number += 10;
+        }
+    }
+}
