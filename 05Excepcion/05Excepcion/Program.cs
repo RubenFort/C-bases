e448a1be7a2820094484a8454a3eb20a932b6351
RubenFort <rdfortu@gmail.com>
2021-07-28 02:31:41 +0200

    Excepciones y throw

---
+++ 05Excepcion/05Excepcion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _05Excepcion
+{
+    class Program
+    {   
+        /// <summary>
+        /// Menu Principal
+        /// </summary>
+        /// <param name="args"> Descripcion del argumento args </param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un numero entero: ");
+            try
+            {
+                int num = Int32.Parse(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                if (e.Message != null)
+                {
+                    throw;
+                }
+                else
+                {
+                    Console.WriteLine("Excepcion: " + e.Message);
+                }
+            }
+            catch (FieldAccessException)
+            {
+                throw;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
