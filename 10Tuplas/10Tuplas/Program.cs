89ced99da0c7c79c5bc9b7ff76cff12a7dc661bd
RubenFort <rdfortu@gmail.com>
2021-08-01 01:14:29 +0200

    Tuplas y Lambdas

---
+++ 10Tuplas/10Tuplas/Program.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+
+namespace _10Tuplas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proveedor1 = ("Pepe", "Vara", 33);
+            Console.WriteLine($"Proveedor: {proveedor1.Item1}, {proveedor1.Item2}, {proveedor1.Item3}");
+            
+            var proveedor2 = (Nombre: "Pepe", Apellido: "Vara", Edad: 33);
+            Console.WriteLine($"Edad del proveedor2: {proveedor2.Edad}");
+
+            (string Nombre, string Apellido) miTupla = (Nombre: "Pepe", Apellido: "Vara");
+            Console.WriteLine($"miTupla Nombre: {miTupla.Nombre}");
+
+            miTupla.Nombre = "Juan";
+            Console.WriteLine($"miTupla Nombre: {miTupla.Nombre}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 11Lambda/11Lambda/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _11Lambda
+{
+    delegate int Operacion (int x, int y);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operacion op = (a, b) => a * b;
+            var resultado = op(2, 3);
+            Console.WriteLine($"Total: {resultado}");
+
+            Operacion potencia = (a, b) =>
+            {
+                Console.WriteLine($"{a} elevado a la {b} potencia");
+                return (int)Math.Pow(a, b);
+            };
+
+            int n1 = 2;
+            int n2 = 4;
+            int resultadoPot = potencia(n1, n2);
+            Console.WriteLine($"Total: {resultadoPot}");
+
+            Func<int, int, int> pot = (x, y) => (int)Math.Pow(x, y);
+            Console.WriteLine($"Total: {pot(5, 5)}");
+
+            //Func(recibe, recibe, return)
+            Func<string, string, bool> igual = (strA, strB) => strA.Equals(strB);
+            string cadA = "Noemi";
+            string cadB = "noemi";
+            Console.WriteLine($"Son iguales? {igual(cadA, cadB)}");
+
+            Console.ReadKey();
+        }
+    }
+}
