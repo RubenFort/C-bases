8db5ee67dd6ac60bb8a801f6cfebfef0add5918a
RubenFort <rdfortu@gmail.com>
2021-07-31 18:51:55 +0200

    Resgistrar e invocar el evento

--- 08Eventos/08Eventos/Program.cs
+++ 08Eventos/08Eventos/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _08Eventos;
+using System;
 
 namespace _08Eventos
 {
@@ -17,24 +18,44 @@ namespace _08Eventos
         Otros = 3
     }
 
-    // 1 - Declaro delegado
+    //*********************************************************************************************************
+    // 1 - Declaro delegado ***********************************************************************************
     public delegate void CambioFormaPagoHandler(TipoPago tipoPago, TipoAlerta tipoAlerta);
 
-    class Program
+    class Program // Clase suscriptora
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Ingresa una forma de pago: ");
             Console.WriteLine("1- Tarjeta \n2- Transferencia \n3- Otros ");
             string tipoPago = Console.ReadLine();
+
+            FormaDePago fm = new FormaDePago();
+
+    //*********************************************************************************************************
+    // 4 - Registro el evento *********************************************************************************
+            fm.CambioFormaPago += fm_seleccionFormaPago;
+
+            fm.Tipo = (TipoPago)Enum.Parse(typeof(TipoPago), tipoPago);
+
+            Console.ReadKey();
         }
+        static void fm_seleccionFormaPago(TipoPago tipo, TipoAlerta tipoAlerta)
+        {
+            if (tipoAlerta.Equals(TipoAlerta.Error))
+                Console.WriteLine("Error, elegiste forma de pago incorrecta");
+            else if (tipoAlerta.Equals(TipoAlerta.Exito))
+                Console.WriteLine("Forma de pago seleccionada: {0}", tipo.ToString()); 
+        }
+        
     }
 
     public class FormaDePago // event broadcaster/emisora
     {
         private TipoPago tipo;
 
-        // 2 - Declaro evento como miembro del delegado
+    //*********************************************************************************************************
+    // 2 - Declaro evento como miembro del delegado ***********************************************************
         public event CambioFormaPagoHandler CambioFormaPago;
 
         public TipoPago Tipo
@@ -53,7 +74,9 @@ namespace _08Eventos
                     tipo = value;
                     tipoAlerta = TipoAlerta.Exito;
                 }
-                // 3 - Lanzar evento cuando se asigna un valor a tipo, es decir en set
+
+    //*********************************************************************************************************
+    // 3 - Lanzar evento cuando se asigna un valor a tipo, es decir en set ************************************
                 CambioFormaPago(tipo, tipoAlerta);
             }
         }
