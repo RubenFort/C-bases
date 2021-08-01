0e34b927297e59db25025a9b94b9ef58574caf9d
RubenFort <rdfortu@gmail.com>
2021-08-01 02:34:56 +0200

    Metodos de extension

---
+++ 13MetodosExtension/13MetodosExtension/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _13MetodosExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region linq
+            int?[] miArr = { 10, 100, null, 500, 50, null };
+            var res = miArr.Sum(e => e);
+            Console.WriteLine(res);
+            #endregion
+
+            #region personalizado
+            var coord = new List<int>() { 5, -9 };
+            var inverso = coord.ObtenerInverso();
+            Console.WriteLine($"Inverso de {coord.First()}, {coord.Last()} = {inverso.First()}, {inverso.Last()}");
+
+            var nuevaCoord = coord.MoverACuadrante(3);
+            Console.WriteLine($"Nuevas coordenandas: {nuevaCoord.First()}, {nuevaCoord.Last()}");
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
